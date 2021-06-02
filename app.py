41cea1c712669b2e3a8101f31e1f9b304a244aea
Jerry Hsu <Jerry>
2021-06-02 18:11:07 -0400

    Part 2: Listing, Getting & Creating Cupcakes

--- app.py
+++ app.py
@@ -10,3 +10,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secret-is-as-secret-does'
 
 connect_db(app)
+
+@app.route('/api/cupcakes')
+def get_cupcakes():
+    cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes = cupcakes)
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcake():
+    req = request.json
+    new_cupcake = Cupcake(
+                flavor = req['flavor'],
+                size = req['size'],
+                rating = req['rating'],
+                image = req['image']
+    )
+    db.session.add(new_cupcake)
+    db.session.commit()
+    return (jsonify(cupcake=new_cupcake.serialize()), 201)
--- models.py
+++ models.py
@@ -13,9 +13,19 @@ class Cupcake(db.Model):
     """ Cupcake Model """
 
     __tablename__ = "cupcakes"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     flavor = db.Column(db.Text, nullable=False)
     size = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, nullable=False,
                       default='https://tinyurl.com/demo-cupcake')
+
+    def serialize(self):
+        return {
+          'id': self.id,
+          'flavor': self.flavor,
+          'size': self.size,
+          'rating': self.rating,
+          'image': self.image
+        }
